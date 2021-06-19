5b4087689e9dce52ec2748941e3d25a7346220c4
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-06-19 15:18:36 +0200

    Add composer.json, Add LatusServiceProvider.php

---
+++ src/LatusServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Latus\Latus;
+
+use Illuminate\Support\ServiceProvider;
+
+class LatusServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+}
