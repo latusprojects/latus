5259fe48ca6d90725ab873e8e43921223512781b
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-16 02:04:07 +0200

    Update ComposerRepositorySeeder.php and RoleSeeder.php seeders with values

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,9 +3,22 @@
 namespace Latus\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Latus\Permissions\Services\RoleService;
 
 class RoleSeeder extends Seeder
 {
+
+    public function __construct(
+        protected RoleService $roleService
+    )
+    {
+    }
+
+    public const ROLES = [
+        ['name' => 'admin', 'level' => 65535],
+        ['name' => 'user', 'level' => 50]
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -13,6 +26,8 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (self::ROLES as $role) {
+            $this->roleService->createRole($role);
+        }
     }
 }
