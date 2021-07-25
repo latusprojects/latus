cef70292cca0c2fb1034a3191a9e04eac9407f58
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-26 00:45:29 +0200

    Add Controller.php and AdminController.php

---
+++ src/Http/Controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Latus\Latus\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function show(Request $request,)
+    {
+
+    }
+}
