1a21faacf21fd4322b6ee57fb1aaed01f7583449
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-08-03 14:31:34 +0200

    Add 'showPage' method to AdminController.php

--- src/Http/Controllers/AdminController.php
+++ src/Http/Controllers/AdminController.php
@@ -4,12 +4,32 @@
 namespace Latus\Latus\Http\Controllers;
 
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+use Latus\UI\Components\Contracts\ModuleComponent;
+use Latus\Latus\Modules\Contracts\AdminModule;
+use Latus\UI\Services\ComponentService;
 
 class AdminController extends Controller
 {
-    public function show(Request $request,)
+    public function showPage(Request $request, ComponentService $componentService, View $viewTarget): Response|View
     {
+        try {
+            /**
+             * @var ModuleComponent $module
+             */
+            $module = $componentService->getActiveModule(AdminModule::class);
+        } catch (BindingResolutionException $e) {
+            return response('Service Unavailable', 503);
+        }
+
+        try {
+            return $module->resolvesView()->with(['admin-nav' => app()->make('admin-nav'), 'content' => $viewTarget->render()]);
+        } catch (\Throwable $e) {
+            return response('Service Unavailable', 503);
+        }
 
     }
 }
