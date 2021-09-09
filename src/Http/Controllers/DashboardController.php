62bd26f0929071b83724008967f1f0f9d41d3a55
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-09 14:36:56 +0200

    Add view for 'showOverview' method

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -2,22 +2,25 @@
 
 namespace Latus\Latus\Http\Controllers;
 
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Latus\Latus\Contracts\Dashboard;
 use Illuminate\View\View;
-use Latus\Latus\Http\Requests\AuthenticateUserRequest;
-use Latus\Latus\Http\Requests\StoreUserRequest;
-use Latus\Latus\Modules\Contracts\AuthModule;
-use Latus\Permissions\Services\UserService;
-use Latus\UI\Components\Contracts\ModuleComponent;
-use Latus\UI\Services\ComponentService;
 
 class DashboardController extends AdminController
 {
-    public function showOverview()
+
+    /**
+     * Show the overview dashboard.
+     *
+     * @return View
+     *
+     * @throws AuthorizationException
+     */
+    public function showOverview(): View
     {
-        
+        $this->authorize('view', [Dashboard::class, 'overview']);
+
+        return $this->returnView(\view('latus::admin.dashboard.overview'), 'dashboard.overview');
     }
 }
