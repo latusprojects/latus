2a9ad80b857ea85a5d92bdec325d2fb7177a82a7
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-09 13:17:23 +0200

    Add initial routes for web and admin modules

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Latus\Latus\Http\Controllers\DashboardController;
 use Latus\Latus\Http\Controllers\WebController;
+use Latus\Latus\Http\Middleware\VerifyUserCanViewAdminModule;
 
 /*
  * Module: Web
  * Routes for basic functionality
  */
-
 Route::middleware(['web'])->group(function () {
-    Route::get('/page/{page_id}', [WebController::class, 'showPage']);
+
+    $webRoutes = function () {
+        Route::get('/page/{page_id}', [WebController::class, 'showPage']);
+    };
+
+    $webRoutesPrefix = config('latus-routes.web_routes_prefix');
+    if ($webRoutesPrefix && $webRoutesPrefix !== '') {
+        Route::prefix($webRoutesPrefix)->group($webRoutes);
+    } else {
+        $webRoutes();
+    }
+});
+
+/*
+ * Module: Admin
+ * Routes for basic functionality
+ */
+Route::middleware(['web', VerifyUserCanViewAdminModule::class])->group(function () {
+
+    $adminRoutesPrefix = config('latus-routes.admin_routes_prefix');
+
+    Route::prefix($adminRoutesPrefix)->group(function () {
+        Route::get('/dashboard/overview', [DashboardController::class, 'showOverview']);
+    });
 });
