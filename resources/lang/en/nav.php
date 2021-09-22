67b5d5cbbe0a3307aef1ead21d021dc35a87f638
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-22 22:48:52 +0200

    Add additional localization strings for navigation

--- resources/lang/en/nav.php
+++ resources/lang/en/nav.php
@@ -1,17 +1,40 @@
 <?php
 
 return [
-    'dashboard' => 'Dashboards',
-    'dashboard.overview' => 'Overview',
-    'dashboard.statistics' => 'Statistics',
+        'dashboard' => 'Dashboards',
+        'dashboard.overview' => 'Overview',
+        'dashboard.statistics' => 'Statistics',
+    ]
+    +
+    [
+        'content' => 'Content & Marketing',
 
-    'content' => 'Content & Marketing',
-    'content.pages' => 'Pages',
-    'content.posts' => 'Posts',
-    'content.events' => 'Events',
-    'content.settings' => 'Settings',
+        'content.pages' => 'Pages',
+        'content.page.index' => 'Overview',
+        'content.page.create' => 'Create',
 
-    'administration' => 'Administration',
-    'admin.rolesAndUsers' => 'Roles & Users',
-    'admin.settingsAndPackages' => 'Configuration, Themes & Plugins'
-];
+        'content.posts' => 'Posts',
+        'content.post.index' => 'Overview',
+        'content.post.create' => 'Create',
+
+        'content.events' => 'Events',
+        'content.event.index' => 'Overview',
+        'content.event.create' => 'Create',
+
+        'content.settings' => 'Settings',
+    ]
+    +
+    [
+        'administration' => 'Administration',
+
+        'admin.rolesAndUsers' => 'Roles & Users',
+        'role.index' => 'Overview: Role',
+        'role.create' => 'Create: Role',
+        'user.index' => 'Overview: User',
+        'user.create' => 'Create: User',
+
+        'admin.settingsAndPackages' => 'Configuration, Themes & Plugins',
+        'plugin.index' => 'Plugins',
+        'theme.index' => 'Themes & Design',
+        'setting.index' => 'Configuration',
+    ];
