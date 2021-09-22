6473394a649f7926b10a9f760f880ea2d8fd5211
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-22 22:27:32 +0200

    Add initial localization support for navigations

--- resources/lang/en/nav.php
+++ resources/lang/en/nav.php
@@ -1,4 +1,17 @@
 <?php
 
 return [
+    'dashboard' => 'Dashboards',
+    'dashboard.overview' => 'Overview',
+    'dashboard.statistics' => 'Statistics',
+
+    'content' => 'Content & Marketing',
+    'content.pages' => 'Pages',
+    'content.posts' => 'Posts',
+    'content.events' => 'Events',
+    'content.settings' => 'Settings',
+
+    'administration' => 'Administration',
+    'admin.rolesAndUsers' => 'Roles & Users',
+    'admin.settingsAndPackages' => 'Configuration, Themes & Plugins'
 ];
